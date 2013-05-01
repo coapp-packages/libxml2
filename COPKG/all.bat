d917236348715d6f23b96d823416d4352c284fbf
Tim Rogers <tiroger@microsoft.com>
2013-05-01 12:11:45 -0700

    added build files

---
+++ COPKG/all.bat
@@ -0,0 +1,47 @@
+@echo off
+setlocal
+
+if "%1"=="clean" goto :clean
+if "%1"=="noclean" (
+	set __NOCLEAN__=true
+	shift)
+
+setlocal
+call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" amd64
+call :build x64 Release v110
+call :build x64 Debug v110
+endlocal
+
+setlocal
+call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x86
+call :build Win32 Release v110
+call :build Win32 Debug v110
+endlocal
+
+setlocal
+call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" amd64
+call :build x64 Release v100
+call :build x64 Debug v100
+endlocal
+
+setlocal
+call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86
+call :build Win32 Release v100
+call :build Win32 Debug v100
+endlocal
+
+if "%__NOCLEAN__%"=="true" goto :eof
+
+goto :clean
+
+:build
+msbuild /P:Platform=%1 /P:Configuration=%2 /P:PlatformToolset=%3 /P:ConfigurationType=DynamicLibrary /P:CallingConvention=Cdecl libxml2\libxml2.vcxproj
+msbuild /P:Platform=%1 /P:Configuration=%2 /P:PlatformToolset=%3 /P:ConfigurationType=StaticLibrary /P:CallingConvention=Cdecl libxml2\libxml2.vcxproj
+msbuild /P:Platform=%1 /P:Configuration=%2 /P:PlatformToolset=%3 /P:ConfigurationType=DynamicLibrary /P:CallingConvention=stdcall libxml2\libxml2.vcxproj
+msbuild /P:Platform=%1 /P:Configuration=%2 /P:PlatformToolset=%3 /P:ConfigurationType=StaticLibrary /P:CallingConvention=stdcall libxml2\libxml2.vcxproj
+goto :eof
+
+:clean
+rd /s /q libxml2\v100
+rd /s /q libxml2\v110
+
